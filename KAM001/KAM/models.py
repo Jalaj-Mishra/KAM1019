28489727e6f33673a45f6da0b20d3ac7c7e71adc
JALAJ MISHRA <mjalaj77@gmail.com>
2024-12-25 20:53:31 +0530

    models implementation started

--- KAM001/KAM/models.py
+++ KAM001/KAM/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Leads(models.Model):
+    RESTAURENT_LEAD_TYPE = [
+        ('BE', 'BinaknerExpress'),
+        ('BB', 'BinaknerBistro'),
+        ('BR', 'BinaknerRestaurant'),
+        ('BC', 'BinaknerCafé'),
+        ('BP', 'BinaknerPizzeria'),
+        ('BB', 'BikanerWala'),
+    ]
+    LEAD_STATUS = [
+        ('OO', 'Open'),
+        ('OC', 'On-Call')
+        ('CE', 'Close'),
+        ('DE', 'Done'),
+    ]
