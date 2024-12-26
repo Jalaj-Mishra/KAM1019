84707b0289b0ecdb2ccf710468d9256f1e1825aa
Jalaj-Mishra <mjalaj77@gmail.com>
2024-12-26 17:29:21 +0530

    Model setup with admin

--- KAM001/KAM/models.py
+++ KAM001/KAM/models.py
@@ -1,18 +1,28 @@
 from django.db import models
 
 # Create your models here.
-class Leads(models.Model):
-    RESTAURENT_LEAD_TYPE = [
+class Lead(models.Model):
+    LEAD_TYPE = [
         ('BE', 'BinaknerExpress'),
         ('BB', 'BinaknerBistro'),
         ('BR', 'BinaknerRestaurant'),
-        ('BC', 'BinaknerCafé'),
+        ('BC', 'BinaknerCafe'),
         ('BP', 'BinaknerPizzeria'),
         ('BB', 'BikanerWala'),
     ]
     LEAD_STATUS = [
         ('OO', 'Open'),
-        ('OC', 'On-Call')
+        ('OC', 'On-Call'),
         ('CE', 'Close'),
         ('DE', 'Done'),
     ]
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=40, primary_key=True)
+    Mobile = models.CharField(max_length=13)
+    LeadType = models.CharField(max_length=2, choices=LEAD_TYPE)
+    LeadStatus = models.CharField(max_length=2, choices=LEAD_STATUS)
+
+
+
+    def __str__(self):
+        return f'{self.name} - {self.LeadType} - {self.LeadStatus}'
